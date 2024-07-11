7a2560cfa89ed6551b0ef519bcb33568a16878c0
NitroSniper <mahiemiah35@gmail.com>
2024-07-11 22:32:39 +0100

    added foliate and hyprshot

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -28,7 +28,9 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
+    foliate
+    hyprshot
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
