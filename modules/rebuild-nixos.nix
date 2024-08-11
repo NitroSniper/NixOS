8d42d3a923c695fec2e50c6d2ca5832c9e04db47
NitroSniper <mahiemiah35@gmail.com>
2024-08-12 00:00:44 +0100

    143 current  2024-08-12 00:00:40  24.11.20240731.9f918d6  6.6.43                          *

--- modules/rebuild-nixos.nix
+++ modules/rebuild-nixos.nix
@@ -1,35 +1,38 @@
 { pkgs, ... }:
 let
   flake-target = ".#default";
+  config-location = "~/nixos/";
   rebuild-nix = pkgs.writeShellScriptBin "rebuild-nixos" ''
     # A rebuild script that commits on a successful build
-    set -e
 
     # cd to your config dir
-    pushd ~/nixos/
+    pushd ${config-location}
 
+    # Edit your config via default editor
+    $EDITOR .
 
-    # Edit your config and If you quit Neovim via :cq then it won't rebuild
-    if [ $EDITOR . -ne 0 ]; then
-      echo "Cancelling rebuild step..."
-      exit 1
+    # If you quit Neovim via :cq then it will skip rebuild step
+    if [ $? -ne 0 ]; then
+      echo "Cancelling rebuild step" && exit 1
     fi
 
     # Print a git diff of changes. 
     git diff -U0 '*.nix'
 
-    # Add any files not tracked by git
-    git add *
-
-    if [ nixfmt --check --quiet . -ne 0]; then
+    nixfmt --check --quiet . 
+    if [ $? -ne 0 ]; then
       echo "Nix files not formatted. Formatting now..."
       nixfmt --quiet . || ( nixfmt . ; echo "formatting failed!" && exit 1)
     fi
 
+    # Add any files not tracked by git
+    echo "Adding all files in ${config-location} to git"
+    git add *
+
     echo "NixOS Rebuilding... with Flake Option: ${flake-target}"
 
     # Rebuild, output simplified errors, log trackebacks
-    sudo nixos-rebuild switch --flake ${flake-target} &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+    sudo nixos-rebuild switch --flake ${flake-target} |& tee nixos-switch.log || (cat nixos-switch.log | rg --color error && exit 1)
 
     current=$(nixos-rebuild list-generations | rg current)
     git commit -am "$current"
