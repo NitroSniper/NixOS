c86dc809026f2c7dd2ccd6d8b228283a05dec0b6
NitroSniper <mahiemiah35@gmail.com>
2024-08-21 01:07:32 +0100

    174 current  2024-08-21 01:07:28  24.11.20240814.c3aa7b8  6.6.45                          *

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -4,7 +4,7 @@
   home.packages = [ (import ./hyprland-gamemode.nix { inherit pkgs; }) ];
   home.pointerCursor = {
     package = pkgs.catppuccin-cursors.mochaDark;
-    name = "mocha-dark";
+    name = "catppuccin-mocha-dark-cursors";
     size = 24;
     gtk.enable = true;
     x11.enable = true;
@@ -46,7 +46,7 @@
       env = [
         "XCURSOR_SIZE, 24"
         "HYPRCURSOR_SIZE, 24"
-        "HYPRCURSOR_THEME, mocha-dark"
+        "HYPRCURSOR_THEME, catppuccin-mocha-dark-cursors"
       ];
 
       #####################
