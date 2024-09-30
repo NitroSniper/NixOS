217714791441ecd8d5d8512c4a9df04a51180001
NitroSniper <mahiemiah35@gmail.com>
2024-09-30 16:33:08 +0100

    199 current  2024-09-30 16:33:06  24.11.20240916.99dc878  6.6.51                          *

--- modules/hyprland-maxtile.nix
+++ modules/hyprland-maxtile.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "hyprland-maxtile" ''
-  HYPRMAXTILE=$(hyprctl getoption general:gaps_in | awk 'NR==1{print $2}')
-  if [ "$HYPRMAXTILE" = 1 ] ; then
+  HYPRMAXTILE=$(hyprctl getoption general:gaps_in | awk 'NR==1{print $3}')
+  if [ "$HYPRMAXTILE" ~= 0 ] ; then
       hyprctl --batch "\
           keyword general:gaps_in 0;\
           keyword general:gaps_out 0;\
