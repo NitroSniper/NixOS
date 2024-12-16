1b03a280e3a44ba92eeac802d407d9fe308b6787
NitroSniper <mahiemiah35@gmail.com>
2024-12-16 11:01:57 +0000

    207 current  2024-12-16 11:01:55  25.05.20241213.3566ab7  6.6.64                          *

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,11 +1,16 @@
-{ pkgs, inputs, ... }:
+{
+  pkgs,
+  inputs,
+  config,
+  ...
+}:
 {
   programs.neovim = {
     enable = true;
   };
 
   # Grab my neovim config
-  xdg.configFile.nvim.source = inputs.neovim-ortin;
+  xdg.configFile.nvim.source = config.lib.file.mkOutOfStoreSymlink inputs.neovim-ortin;
 
   # Needed for telescope search
   home.packages = [ pkgs.ripgrep ];
